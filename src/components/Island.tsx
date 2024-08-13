e053c394d1bb85fdc65e45ac70bb56803cf098e0
freddie <freddie@DESKTOP-TNSC6RN>
2024-08-13 17:50:01 +0100

    add threejs example

--- src/components/Island.tsx
+++ src/components/Island.tsx
@@ -1,20 +1,53 @@
-import { useEffect } from "react";
+import React, { useRef, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
+
+interface BoxProps {
+  position: [number, number, number];
+}
+
+const Box = (props: BoxProps) => {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<Mesh>(null);
+  
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += delta;
+    }
+  });
+  
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
 
 const Island = () => {
-    const callAPI = async () => {
-        const data = await fetch('https://api-football-v1.p.rapidapi.com/v2/odds/league/865927/bookmaker/5?page=2')
-        console.log(data.body)
-    };
-
-    useEffect(() => {
-        callAPI();
-    },[]);
-
     return (
-        <div>
-            This is an island
-        </div>
-    )
+        <Canvas>
+            <ambientLight intensity={Math.PI / 2} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    );
 };
 
 export default Island;
